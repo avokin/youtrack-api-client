39729422eb16e09c2e2ac3e83f8d95647a59e3ea
Andrey Vokin <andrey.vokin@jetbrains.com>
2020-04-23 08:10:28 +0200

    Update snapshot strategies

--- youtrack/first_assignee_snapshot_strategy.py
+++ youtrack/first_assignee_snapshot_strategy.py
@@ -1,13 +1,6 @@
-from youtrack.snapshot_strategy import SnapshotStrategy
+from youtrack.issue_created_snapshot_strategy import IssueCreatedSnapshotStrategy
 
 
-class FirstAssigneeSnapshotStrategy(SnapshotStrategy):
+class FirstAssigneeSnapshotStrategy(IssueCreatedSnapshotStrategy):
     def is_snapshot_taken(self, issue):
         return 'first_assignee' in issue
-
-    def process(self, issue):
-        super().process(issue)
-
-        snapshot_issue = self.issues[issue['id']]
-        if 'assignee' not in snapshot_issue and 'assignee' in snapshot_issue:
-            snapshot_issue['first_assignee'] = snapshot_issue['assignee']
