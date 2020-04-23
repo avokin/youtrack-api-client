48ff673e0b2dd10042c131db85fd5db5b0517037
Andrey Vokin <andrey.vokin@jetbrains.com>
2020-04-23 08:12:11 +0200

    Support "subsystem" in IDEA project

--- youtrack/idea_activity_manager.py
+++ youtrack/idea_activity_manager.py
@@ -5,4 +5,5 @@ class IdeaActivityManager(ActivityManager):
     def __init__(self, snapshot_strategy):
         super().__init__(snapshot_strategy,
                          custom_field_mapping={'__CUSTOM_FIELD__State_25': {'name': 'state', 'field': 'name'},
-                                               '__CUSTOM_FIELD__Assignee_30': {'name': 'assignee', 'field': 'login'}})
+                                               '__CUSTOM_FIELD__Assignee_30': {'name': 'assignee', 'field': 'login'},
+                                               '__CUSTOM_FIELD__Subsystem_26': {'name': 'subsystem', 'field': 'name'}})
